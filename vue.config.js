2e3a5460ff9314fab129e7871afcce8a9920336b
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2023-02-24 10:51:25 +0700

    removes public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
-// const { defineConfig } = require('@vue/cli-service')
-module.exports =  { 
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/employee-vue/'
-    : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/employee-vue/'
+  //   : '/'
+})
